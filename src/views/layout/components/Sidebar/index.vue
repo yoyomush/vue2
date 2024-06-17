b5d81cbd0483caa665e69f899f4aa843f2d7b5f8
yoyomush <qixiangyi_eva@163.com>
2024-06-17 12:29:09 +0800

    Update index.vue

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -1,40 +1,52 @@
 <template>
   <el-scrollbar wrap-class="scrollbar-wrapper">
+
     <el-menu
       :default-active="$route.path"
       :collapse="isCollapse"
-      :background-color="variables.menuBg"
-      :text-color="variables.menuText"
-      :active-text-color="variables.menuActiveText"
+      :background-color="vars.menuBg"
+      :text-color="vars.menuText"
+      :active-text-color="vars.menuActiveText"
       :collapse-transition="false"
       mode="vertical"
     >
-      <sidebar-item v-for="route in permission_routes" :key="route.path" :item="route" :base-path="route.path"/>
+      <SidebarItem v-for="route in filteredRoutes" :key="route.path" :item="route" :base-path="route.path" :is-collapse="isCollapse"/>
     </el-menu>
   </el-scrollbar>
 </template>
 
-<script>
-import { mapGetters } from 'vuex'
-import variables from '@/styles/variables.scss'
-import SidebarItem from './SidebarItem'
+<script setup>
+import { computed, watch } from 'vue'
+import { usePermissionStore } from '@/stores/permission'
+import SidebarItem from './SidebarItem.vue'
 
-export default {
-  components: { SidebarItem },
-  computed: {
-    ...mapGetters([
-      'permission_routes',
-      'sidebar'
-    ]),
-    routes() {
-      return this.$router.options.routes
-    },
-    variables() {
-      return variables
-    },
-    isCollapse() {
-      return !this.sidebar.opened
-    }
-  }
+const store = usePermissionStore()
+
+// 过滤掉 hidden 的路由
+const filteredRoutes = computed(() => {
+  return store.routes.filter(route => !route.meta?.hidden)
+})
+
+const sidebar = computed(() => store.sidebar)
+const isCollapse = computed(() => sidebar.value ? !sidebar.value.opened : true)
+
+const vars = {
+  menuBg: getComputedStyle(document.documentElement).getPropertyValue('--menu-bg').trim(),
+  menuText: getComputedStyle(document.documentElement).getPropertyValue('--menu-text').trim(),
+  menuActiveText: getComputedStyle(document.documentElement).getPropertyValue('--menu-active-text').trim()
 }
+
+// 观察 sidebar 变化
+watch(sidebar, (newVal, oldVal) => {
+  console.log('sidebar changed:', oldVal, '->', newVal)
+})
+watch(isCollapse, () => {
+  console.log(isCollapse.value, sidebar.value)
+})
+
 </script>
+
+<style scoped lang="scss">
+@import "@/styles/variables.scss";
+
+</style>
